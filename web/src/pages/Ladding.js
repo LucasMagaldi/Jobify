7a717c86ee283e25cae9703d96eeadd7bc381f2b
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-03-02 17:16:18 -0300

    images and ladding page

---
+++ web/src/pages/Ladding.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+export const Ladding = () => {
+  return (
+    <main>
+        <nav>
+            <img src={logo} alt='logo' className='logo'/>
+        </nav>
+        <div className='container page'>
+            <div className='info'>
+                <h1>
+                    Job <span>Tracking</span> App
+                </h1>
+                <p>
+                    Web plataform to manage yours best job positons.
+                    Met the best companies in your country and world.
+                    Apply to position that companies with you and be happy.
+                </p>
+                <button className='btn btn-hero'>Login/Register</button>
+            </div>
+            <img src={main} alt='job hunt' className='img main-img'/>
+        </div>
+    </main>
+  )
+}
+
+export default Ladding;
