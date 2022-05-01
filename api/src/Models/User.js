0c1a1b04415d87e9321b2078cdd61170c856005d
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-05-01 20:26:30 -0300

    Hash Pw

--- api/src/Models/User.js
+++ api/src/Models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 import validator from 'validator'
 
 const UserSchema = new mongoose.Schema({
@@ -37,5 +38,9 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
+UserSchema.pre('save', async () => {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 export default mongoose.model('User', UserSchema);
